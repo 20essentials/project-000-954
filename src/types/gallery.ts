1ca0193b6f7227aeffce24adaaa00bbbaf1a3f99
DominicDev <s0l0estudi4r@gmail.com>
2025-05-28 19:00:04 +0200

    v0.0.1

---
+++ src/types/gallery.ts
@@ -0,0 +1,8 @@
+export type Masory<T> = T & {gap:string, maxcolwidth:string}
+declare global {
+  namespace preact.createElement.JSX {
+    interface IntrinsicElements {
+      ['masonry-layout']: Masory<JSX.HTMLAttributes>
+    }
+  }
+}
